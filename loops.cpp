3159c1535ac2f199aedf305e91cbd35d65bfe886
Sylvester Yiadom <sylvesteryiadom@gmail.com>
2020-05-18 11:05:18 -0500

    second commit

---
+++ loops.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string response;
+    // int i;
+    // for (i = 1; i < 11; i++)
+    // {
+    //     cout << "Similar patterns for forloops\n";
+    // }
+
+    // while (!response == 'Quit')
+    // {
+    //     cout << "Enter menu choice " << endl
+    //          << "More" << endl
+    //          << "Quit" << endl;
+    //     cin >> response;
+    // }
+}
