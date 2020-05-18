8df16ce13adef2289f1cf3da0ebbd476c2c6c055
Sylvester Yiadom <sylvesteryiadom@gmail.com>
2020-05-18 10:21:50 -0500

    second commit

---
+++ ternary.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 1, j = 2;
+    // ternary operator checking for whether i is greater than j. ouputs the greater number
+    cout << (i > j ? i : j) << " is greater\n";
+}
